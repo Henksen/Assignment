360e91a4b8f88445a70d6f1fbded00e245a8a5c4
Pierik <henk-jan.pierik@rabobank.nl>
2022-01-07 11:39:50 +0100

    Forgot to start it, also added jpa annotation

--- spotify-freemium/src/main/java/spotify/Application.java
+++ spotify-freemium/src/main/java/spotify/Application.java
@@ -1,12 +1,15 @@
 package spotify;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Running..." );
+        SpringApplication.run(Application.class, args);
     }
 }
