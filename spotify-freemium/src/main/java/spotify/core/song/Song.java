d2f20e4d68266cdbbd4801063a4fe42f9c1984ac
Pierik <henk-jan.pierik@rabobank.nl>
2022-01-07 12:23:49 +0100

    debugged, now works

--- spotify-freemium/src/main/java/spotify/core/song/Song.java
+++ spotify-freemium/src/main/java/spotify/core/song/Song.java
@@ -1,5 +1,7 @@
 package spotify.core.song;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -8,15 +10,26 @@ import lombok.Getter;
 @Getter
 @EqualsAndHashCode
 public class Song {
+
+    @JsonProperty("Id")
     private final int id;
+    @JsonProperty("Name")
     private final String name;
+    @JsonProperty("Year")
     private final int year;
+    @JsonProperty("Artist")
     private final String artist;
+    @JsonProperty("ShortName")
     private final String shortName;
+    @JsonProperty("Bpm")
     private final int bpm;
+    @JsonProperty("Duration")
     private final int duration;
+    @JsonProperty("Genre")
     private final String genre;
+    @JsonProperty("SpotifyId")
     private final String spotifyId;
+    @JsonProperty("Album")
     private final String album;
 
 }
