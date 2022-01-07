d28537212cac962727889c46218d40f1f5d07dee
Pierik <henk-jan.pierik@rabobank.nl>
2022-01-07 10:41:21 +0100

    Created monstrous unit test that doesn't work yet

---
+++ spotify-freemium/src/test/java/spotify/core/artist/ArtistServiceTest.java
@@ -0,0 +1,72 @@
+package spotify.core.artist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import spotify.core.artist.mapper.ToArtistEntityMapper;
+import spotify.core.artist.repo.ArtistEntity;
+import spotify.core.artist.repo.ArtistRepository;
+
+@ExtendWith(MockitoExtension.class)
+class ArtistServiceTest {
+
+    @Mock
+    private ArtistRepository repository;
+
+    @Mock
+    private ToArtistEntityMapper toArtistEntityMapper;
+
+    @InjectMocks
+    private ArtistService sut;
+
+    @Test
+    void when_add_artists_only_not_existing_artist_persisted() {
+        final Artist artist1 = Artist.builder()
+                .name("artist_1")
+                .id(1)
+                .build();
+        final Artist artist2 =  Artist.builder()
+                .name("artist_2")
+                .id(2)
+                .build();
+
+        final List<Artist> artists = List.of(artist1, artist2);
+
+        ArtistEntity artistEntity1 = new ArtistEntity();
+        artistEntity1.setId(1);
+        artistEntity1.setName("artist_1");
+
+        ArtistEntity artistEntity2 = new ArtistEntity();
+        artistEntity1.setId(2);
+        artistEntity1.setName("artist_2");
+
+        lenient().when(toArtistEntityMapper.map(artist1)).thenReturn(artistEntity1);
+        lenient().when(toArtistEntityMapper.map(artist2)).thenReturn(artistEntity2);
+
+        when(repository.existsById(artist1.getId())).thenReturn(false);
+        when(repository.existsById(artist2.getId())).thenReturn(true);
+
+        sut.addArtists(artists);
+
+        verify(repository).existsById(artistEntity1.getId());
+        verify(repository).existsById(artistEntity2.getId());
+
+        verify(repository).save(artistEntity1);
+    }
+
+
+}
